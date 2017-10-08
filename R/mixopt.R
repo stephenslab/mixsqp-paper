fb5677f2f616c04c816910d144535eddd991ffcb
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-10-08 15:38:17 -0500

    Added descriptions of some of the mixsqp() inputs.
    Former-commit-id: 88e0f7c2765ccb4382816e75f2d6b06a6d4fc190

--- R/mixopt.R
+++ R/mixopt.R
@@ -1,11 +1,44 @@
-# TO DO: Fill out roxygen2 documentation.
-#
 #' @title SQP algorithm with partial QR for optimizing mixtures.
 #' 
 #' @description Sequential quadratic programming (SQP) method with
 #'     partial QR-based approximate computation of gradients and Hessians
 #'     for efficiently solving the mixture distribution optimization
 #'     problem.
+#'
+#' @param L \eqn{n x k} data matrix with positive entries. For fitting
+#'     mixture models, n is the number of samples and k is the number of
+#'     mixture components, and the entries of L are the conditional
+#'     likelihoods for each sample (row) and each mixture component
+#'     (column).
+#'
+#' @param x Vector of length k containing the initial estimate of the
+#'     solution. For fitting mixture models, x is the initial estimate of
+#'     the mixture weights.
+#'
+#' @param convtol Tolerance for stopping criterion in SQP method.
+#'    TO DO: explain more precisely what the stopping criterion is.
+#'
+#' @param pqrtol Relative precision used to determine number of
+#'    factors in QR decomposition of matrix L. See the LowRankApprox
+#'    Julia package for details.
+#'
+#' @param eps A small positive number used to stabilize some of the
+#'    numerical operations.
+#'
+#' @param sptol All entries of x below this value are treated as zero.
+#'
+#' @param maxiter
+#'
+#' @param maxqpiter
+#'
+#' @param seed
+#'
+#' @param verbose
+#'
+#' @details
+#'     TO DO: Details about the algorithm go here, such as a
+#'     mathematical description of the optimization problem, and a brief
+#'     description of how the SQP algorithm works.
 #' 
 #' @export
 #' @importFrom rjulia r2j
@@ -14,8 +47,17 @@
 mixsqp <- function (L, x, convtol = 1e-8, pqrtol = 0, eps = 1e-8,
                     sptol = 1e-3, maxiter = 100, maxqpiter = 100,
                     seed = 1, verbose = TRUE) {
+
+  # Get the number of mixture components.
+  k <- ncol(L)  
     
-  # Pass arguments from R to Julia.
+  # Set default initial estimate for x.
+  if (missing(x))
+    x <- rep(1/k,k)
+    
+  # Pass arguments from R to Julia. Since r2j treats everything as a
+  # matrix, additional (somewhat painful) steps need to be taken to
+  # pass scalars, including integers and logicals.
   r2j(L,"L")
   r2j(x,"x")
   r2j(convtol,"convtol")
@@ -25,17 +67,15 @@ mixsqp <- function (L, x, convtol = 1e-8, pqrtol = 0, eps = 1e-8,
   r2j(maxiter,"maxiter")
   r2j(maxqpiter,"maxqpiter")
   r2j(seed,"seed")
-  if (verbose)
-    julia_eval("verbose = true;")
-  else
-    julia_eval("verbose = false;")
-  julia_eval("convtol   = convtol[1]")
-  julia_eval("pqrtol    = pqrtol[1]")
-  julia_eval("eps       = eps[1]")
-  julia_eval("sptol     = sptol[1]")
-  julia_eval("maxiter   = Integer(maxiter[1])")
-  julia_eval("maxqpiter = Integer(maxqpiter[1])")
-  julia_eval("seed      = Integer(seed[1])")
+  r2j(verbose,"verbose")
+  julia_eval("convtol   = convtol[1];")
+  julia_eval("pqrtol    = pqrtol[1];")
+  julia_eval("eps       = eps[1];")
+  julia_eval("sptol     = sptol[1];")
+  julia_eval("maxiter   = Integer(maxiter[1]);")
+  julia_eval("maxqpiter = Integer(maxqpiter[1]);")
+  julia_eval("seed      = Integer(seed[1]);")
+  julia_eval("verbose   = verbose[1];")
   
   # Load the Julia code. Note that this code may not work on all
   # systems (e.g., Windows)---in the future it would be better to
@@ -51,9 +91,10 @@ mixsqp <- function (L, x, convtol = 1e-8, pqrtol = 0, eps = 1e-8,
                         "maxqpiter = maxqpiter,seed = seed,verbose = verbose)",
                         sep = ","))
   
-  # Construct the return value. It seems that assigning the individual
-  # dictionary (i.e., list) entries to variables within Julia helps to
-  # prevent errors about memory not being correctly mapped.
+  # Construct the return value. It seems (though not verified) that
+  # assigning the individual "dictionary" (i.e., list) entries to
+  # variables within Julia helps to prevent errors about memory not
+  # being correctly mapped.
   return(list(x         = julia_eval("x         = out[\"x\"];"),
               totaltime = julia_eval("totaltime = out[\"totaltime\"];"),
               obj       = julia_eval("obj       = out[\"obj\"];"),
