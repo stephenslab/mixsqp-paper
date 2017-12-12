6a82972aad1bca1f026d5f9674b1d87b7a94a57f
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-12-11 22:29:25 -0600

    Additional rough code blocks added to mixcvx.R thanks to Youngseok.
    Former-commit-id: f97866e1519f84511e01212c29aeee2569ab905c
    Former-commit-id: 86ce5b5f581be3d3e559555c9f5190d148b03217

--- inst/code/R/mixcvx.R
+++ inst/code/R/mixcvx.R
@@ -7,11 +7,31 @@
 library(CVXR)
 load("../../../data/normmix.data.RData")
 L <- normmix.data$L
-L <- L[,18:20]
 k <- ncol(L)
 x <- Variable(k)
 objective   <- Maximize(sum(log(L %*% x)))
 constraints <- list(x > 0,sum(x) == 1)
 prob        <- Problem(objective,constraints)
-out         <- solve(prob)
+out         <- solve(prob,solver = "SCS")
 out$getValue(x)
+
+n = dim(L)[1]; k = dim(L)[2];
+t_rmosek = system.time(res <- REBayes::KWDual(L, rep(1,k), rep(1,n)/n))[3]
+
+set.seed(2017)
+n = 1000
+m = 1.2
+n = floor(n/2)
+z = c(rnorm(n),4*rt(n,df=6))
+z = z[order(abs(z))]
+data = ashr::set_data(z,1)
+grid = ashr:::autoselect.mixsd(data, mult=m, mode=0,
+    mixcompdist = "normal", grange = c(-Inf,Inf))
+grid = c(0,grid)
+k = length(grid)
+g  = ashr::normalmix(rep(1/k,k),rep(0,k),grid)
+llik <- t(ashr:::log_comp_dens_conv.normalmix(g,data))
+L = llik - apply(llik, 1, max)
+L = exp(L)
+
+print(sum(log(L %*% y)),digits=12)
