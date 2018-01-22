381d678313a804811aa18b7698d872520baf5e74
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-01-22 13:59:14 -0600

    Rcpp code compiles successfully with devtools::document() after adding code & files from RcppArmadillo.package.skeleton(); note there are some warnings related to rjulia package.
    Former-commit-id: 3b48f802387f863dddd624776d91ff7a7913866f
    Former-commit-id: 6778e47f8b3cd66863dcdf0841d98eae2edb872f

--- src/mixsqp.cpp
+++ src/mixsqp.cpp
@@ -2,10 +2,26 @@
 
 // This depends statement is needed to tell R where to find the
 // additional header files.
-// 
+//
 // [[Rcpp::depends(RcppArmadillo)]]
 
-// Some functions and classes uses frequently in the code below.
-using arma::vec;
+using namespace Rcpp;
 
-arma::vec mixsqp_rcpp
+// SQP algorithm for optimizing mixtures. For more information, see
+// the help and comments accompanying the "mixsqp" function in R.
+// 
+// [[Rcpp::export]]
+arma::vec mixsqp_rcpp (const arma::mat& L, const arma::vec& x0,
+		       double convtol, double eps, int maxiter,
+		       bool verbose) {
+
+  // Get the number of rows (n) and columns (k) of the conditional
+  // likelihood matrix.
+  int n = L.n_rows;
+  int k = L.n_cols;
+
+  printf("n = %d, k = %d\n",n,k);
+
+  arma::vec x = x0;
+  return x;
+}
