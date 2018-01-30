1ad95f00a8f9be50e823b232a919a1572097a062
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-01-30 16:45:35 -0600

    Computation of gradient & Hessian is a bit more sensible.
    Former-commit-id: dcd5d0c6033ca29b373a5be8afcafb2ea97f4d07
    Former-commit-id: afdfe7f96d1ae456ea6bc1f9260a71f222b0e49e

--- src/mixsqp.cpp
+++ src/mixsqp.cpp
@@ -22,20 +22,21 @@ Rcpp::List mixsqp_rcpp (const arma::mat& L, const arma::vec& x0,
   int n = L.n_rows;
   int k = L.n_cols;
 
-  // Print a brief summary of the analysis.
-  Rprintf("Running SQP algorithm with the following settings:\n");
-  Rprintf("- %d x %d data matrix\n",n,k);
-  Rprintf("- convergence tolerance = %0.2e\n",convtol);
-  Rprintf("- zero threshold        = %0.2e\n",sptol);
-
+  // Print a brief summary of the analysis, if requested.
+  if (verbose) {
+    Rprintf("Running SQP algorithm with the following settings:\n");
+    Rprintf("- %d x %d data matrix\n",n,k);
+    Rprintf("- convergence tolerance = %0.2e\n",convtol);
+    Rprintf("- zero threshold        = %0.2e\n",sptol);
+  }
+  
   // Initialize the solution.
   arma::vec x = x0;
   
   // Initialize storage for matrices and vectors used in the
-  // optimization algorithm below.
+  // computations below.
   arma::rowvec g(k);
   arma::vec u(n);
-  arma::mat U(n,n);
   arma::mat H(k,k);
   arma::mat Z(n,k);
   arma::mat I(k,k);
@@ -44,18 +45,18 @@ Rcpp::List mixsqp_rcpp (const arma::mat& L, const arma::vec& x0,
   // Repeat until we reach the maximum number of outer loop iterations.
   for (int i = 0; i < maxiter; i++) {
 
-    // Compute the gradient.
+    // Compute Z = diag(1/(L*x + eps)) * L.
     u = L * x + eps;
-    u = 1 / u;
-    U = arma::diagmat(u);
-    Z = U * L;
-    g = arma::sum(Z,0);
-    g = -g / n;
+    Z = L;
+    Z.each_col() /= u;
 
-    // Compute the Hessian.
+    // Compute the gradient g = -Z'*1/n.
+    g = arma::sum(Z,0);
+    g = -g/n;
+
+    // Compute the Hessian H = Z'*Z/n + eps*I.
     H = Z.t() * Z;
-    H = H/n;
-    H = H + I;
+    H = H/n + I;
   }
   
   return List::create(Named("g") = g,Named("H") = H);
