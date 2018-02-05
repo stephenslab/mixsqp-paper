a0e27bb93d0005c79ce73042bc5766a22748a4be
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-02-04 22:28:03 -0600

    Implemented EM algorithm in Julia, and created a small demo to illustrate the algorithm on the 'normmix' data set.
    Former-commit-id: f5bf2f94fa81f4db921bc764548ec347baef8de7
    Former-commit-id: 13efd4a64f4f78263d561806dd4ee520d351b9f0

---
+++ inst/code/julia/mixem.jl
@@ -0,0 +1,54 @@
+# TO DO: Add brief description of function and input arguments here.
+function mixem(L, w; maxiter = 10000, tol = 1e-6, eps = 1e-8)
+
+  # Get the number of rows (n) and columns (k) of the conditional
+  # likelihood matrix.
+  n = size(L,1);
+  k = size(L,2);
+
+  # Initialize storage for outputs obj and maxd.
+  obj  = zeros(maxiter);
+  maxd = zeros(maxiter);
+
+  # Compute the objective function value at the initial iterate.
+  iter      = 1;
+  obj[iter] = -sum(log.(L * w + eps));
+    
+  # Repeat until convergence criterion is met, or until the maximum
+  # number of iterations is reached.
+  for iter = 2:maxiter
+
+    # Save the current estimate of the mixture weights.
+    w0 = w;
+
+    # E STEP
+    # ------                       
+    # Compute the posterior probabilities.
+    P = L * spdiagm(w);
+    P = P ./ repmat(sum(P,2) + eps,1,k);
+
+    # M STEP
+    # ------                       
+    # Update the mixture weights.
+    w = mean(P,1)'[:];
+    
+    # COMPUTE OBJECTIVE
+    # -----------------
+    obj[iter] = -sum(log.(L * w + eps));
+      
+    # CHECK CONVERGENCE
+    # -----------------                       
+    # Print the status of the algorithm and check the convergence
+    # criterion. Convergence is reached when the maximum difference
+    # between the mixture weights at two successive iterations is less
+    # than the specified tolerance, or when objective increases.
+    maxd[iter] = maximum(abs.(w - w0));
+    if maxd[iter] < tol
+      break
+    end
+  end
+      
+  # Return the mixture weights and other optimization info.
+  return w, obj[1:iter], maxd[1:iter]
+end        
+
