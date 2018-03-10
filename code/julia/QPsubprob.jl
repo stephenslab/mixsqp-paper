4dc19f9356e94b835d64a5509986b62fff7e2388
Youngseok Kim <workyoungseok@gmail.com>
2018-03-10 07:01:47 -0600

    update
    Former-commit-id: 21fb259aefd996ed13d5b00259fbedbbbc2d4f1e
    Former-commit-id: 5d433892bc1428beb9b22569c282d4fb3a43cda6

--- code/julia/QPsubprob.jl
+++ code/julia/QPsubprob.jl
@@ -1,59 +1,27 @@
 using Mosek, JuMP
 
-function QPsubprob(L; initialize = "equal", method = "activeset", warmstart = true, lowrank = "svd")
+function QPsubprob(L; method = "activeset")
   # fix setting
-  eps = 1e-8; tol = 1e-8; pqrtol = 1e-10; sptol=1e-3; maxiter = 100;
+  eps = 1e-5; tol = 1e-8; sptol=1e-3; maxiter = 100;
   n = size(L,1); k = size(L,2);
-  if lowrank == "qr"
-      F = pqrfact(L, rtol=pqrtol);
-      P = sparse(F[:P]);
-  elseif lowrank == "svd"
-      F = psvdfact(L, rtol=pqrtol);
-      S = Diagonal(F[:S]);
-  else
-  end
     
   # return param
-  timing = zeros(maxiter); linesearch = zeros(maxiter); i = 0;
+  timing = zeros(maxiter); linesearch = zeros(maxiter);
+  y_nnz = zeros(maxiter); q_nnz = zeros(maxiter); i = 0;
     
   # initialize
-  if initialize == "equal"
-      x = ones(k)/k;
-  elseif initialize == "null"
-      x = sparse(zeros(k)); x[1] = 1;
-  elseif initialize == "sparse"
-      x = sparse(zeros(k)); x[1] = 1/2; x[round(Int,k/2)] = 1/2;
-  else
-      x = rand(k); x = x/sum(x);
-  end
+  x = sparse(zeros(k)); x[1] = 1/2; x[round(Int,k/2)] = 1/2;
 
   # QP subproblem start
   for i = 1:maxiter
     # gradient and Hessian computation -- Rank reduction method
-    if lowrank == "qr"
-        D = 1./(F[:Q]*(F[:R]*(P'*x)) + eps);
-        g = -P * F[:R]' * (F[:Q]'*D)/n;
-        H = P * F[:R]' * (F[:Q]'*Diagonal(D.^2)*F[:Q]) * F[:R] * P'/n + eps * eye(k);
-    elseif lowrank == "svd"
-        D = 1./(F[:U]*(S*(F[:Vt]*x)) + eps);
-        g = -F[:Vt]'*(S * (F[:U]'*D))/n;
-        H = (F[:V]*S*(F[:U]'*Diagonal(D.^2)*F[:U])* S*F[:Vt])/n + eps * eye(k);
-    else
-        D = 1./(L*x + eps);
-        g = -L'*D/n;
-        H = L'*Diagonal(D.^2)*L/n + eps * eye(k);
-    end
+    D = 1./(L*x + eps);
+    g = -L'*D/n;
+    H = L'*Diagonal(D.^2)*L/n + eps * eye(k);
     
     tic();
     if method == "activeset"
-        # initialize working set warmstart or not
-        if warmstart == true
-            ind = find(x .> sptol); # this is inactive set
-            y = sparse(zeros(k)); y[ind] = 1/length(ind);
-        else
-            ind = [1];
-            y = sparse(zeros(k)); y[1] = 1;
-        end
+        y = sparse(zeros(k)); y[1] = 1; ind = [1];
             
         # Active set method start
         for j = 1:100
@@ -99,32 +67,56 @@ function QPsubprob(L; initialize = "equal", method = "activeset", warmstart = tr
             y = y + alpha * p;
           end
         end
-    else
-        # interior point method start
+    elseif method == "mosek2"
+        bkx   = repmat([MSK_BK_LO], k, 1)[:]
+        blx   = zeros(k)
+        bux   = Inf * ones(k)
+        numvar = length(bkx)
+        c     = 2*g+1
+
+        maketask() do task
+            appendvars(task,numvar)
+            putclist(task,[1:numvar;],c)
+            putvarboundslice(task,1,numvar+1,bkx,blx,bux)
+            Q = LowerTriangular(H); ind = (Q .> 0); a,b = findn(ind);
+            putqobj(task,a,b,Q[ind])
+            putobjsense(task,MSK_OBJECTIVE_SENSE_MINIMIZE)
+
+            # Optimize
+            optimize(task)
+
+            y = getxx(task,MSK_SOL_ITR)
+        end
+   else
         mod = Model(solver=MosekSolver(QUIET = true));
-        @variable(mod, y[1:k] >= 0);
-        @NLobjective(mod, Min, sum(y[i]'*H[i,j]*y[j] for i = 1:k, j = 1:k)/2 + sum(y[i]*(2*g[i]+1) for i = 1:k));
-        solve(mod);
-        y = getvalue(y);
+        a,b = findn(H);
+        @variable(mod, u[1:k] >= 0);
+        @objective(mod, Min, QuadExpr(u[a],u[b],H[:]/2,AffExpr(u, 2g+1, 0)) )
+        solve(mod)
+        y = getvalue(u);
     end
     timing[i] = toq();
-    
      
     for linesearch[i] = 1:10
-        if sum(log.(D)) - sum(log.(1./(F[:U]*(S*(F[:Vt]*y)) + eps))) > sum((x-y) .* g) / 2
+        if sum(log.(D)) - sum(log.(1./(L*y + eps))) > sum((x-y) .* g) / 2
             break;
         end
         y = (y-x)/2 + x;
     end
-    x = y;
-
+        
+    y_nnz[i] = sum(y .> sptol);
+    q_nnz[i] = sum(abs.(x-y) .> sptol);
+        
+    x = y + 0.0;
+        
     # convergence check
     if minimum(g+1) >= 0
       break;
     end
   end
+
   x[x .< sptol] = 0; x = x/sum(x);
     
-  return Dict([("numiter", i), ("eachQPtime",timing[1:i]), ("linesearch", linesearch[1:i]),
-            ("totalQPtime", sum(timing[1:i]))])
+  return Dict([("x", full(x)), ("numiter", i), ("eachQPtime",timing[1:i]), ("linesearch", linesearch[1:i]),
+            ("totalQPtime", sum(timing[1:i])), ("y_nnz", y_nnz[1:i]), ("q_nnz", q_nnz[1:i])])
 end
