6fda5cb151034e4bb507f137e38f93df49b47e64
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-03-14 13:24:58 -0500

    Replaced likelihood matrix computation in ash function with normlikmatrix().
    Former-commit-id: dde6129a98aa32bf029d65ea7134515ddaa04a82
    Former-commit-id: 10dded5608c36d6689bc09943657adb0bbb9ca45

--- code/julia/ash.jl
+++ code/julia/ash.jl
@@ -1,28 +1,12 @@
 function ash(x,s; method = "mixSQP", mult = 1.3, lowrank = "svd")
+
+    # Compute the n x m likelihood matrix.
     tic();
-    s2 = s.^2
-    x2 = x.^2;
-    s_min = sqrt(minimum(s2))/10;
-    if all(x2 .<= s2) s_max = 10 * s_min; # to deal with the occassional odd case
-    else s_max = 2 * sqrt(maximum(x2-s2)); # this computes a rough largest value you'd want to use
-    end
+    sd      = autoselectmixsd(x,s,gridmult = mult);
+    log_lik = normlikmatrix(x,s,sd = sd);
+    t1      = toq();
     
-    # choose grid
-    if mult == 0
-        grid = [0;s_max];
-    else 
-        n = ceil(Int, log2(s_max/s_min)/log2(mult));
-        grid = [0;mult.^((-n):0) * s_max];
-    end
-    
-    # matrix likelihood
-    s_matrix = sqrt.((s2) .+ (grid.^2)') # n by m matrix of standard deviations of convolutions
-    log_lik = -(x./s_matrix).^2/2 - log.(s_matrix) - log(2*pi)/2;
-    log_lik = log_lik - repmat(maximum(log_lik,2),1,size(log_lik,2));
-    log_lik = exp.(log_lik);
-    t1 = toq();
-    
-    # fit the model
+    # Fit the model.
     if method == "mixSQP"
         temp = mixSQP_time(log_lik, pqrtol = 1e-8, lowrank = lowrank);
         t3 = temp[3];
@@ -37,11 +21,12 @@ function ash(x,s; method = "mixSQP", mult = 1.3, lowrank = "svd")
     end
     
     tic();
-    # exploit sparsity
+    
+    # Exploit sparsity.
     ind = find(p .> 0);
     ps2 = grid[ind].^2;
     
-    # posterior calculation
+    # Posterior calculations.
     temp = s2 .+ ps2';
     comp_post_mean = (x * ps2') ./ temp;
     comp_post_sd2 = (s2 * ps2') ./ temp;
