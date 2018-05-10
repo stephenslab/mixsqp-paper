f806f19939cf4ff2b23f94f1fac3d7988b8b1122
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-05-10 16:48:59 -0500

    mixSQP doesn't convergence in mixSQP_example notebook when lowrank = none.
    Former-commit-id: 03f1ae49f5117c3e58ccf2d4a2107d6d66968fcc
    Former-commit-id: c17738551d82b55345eb37225254a0513692092b

--- code/julia/REBayes.jl
+++ code/julia/REBayes.jl
@@ -1,7 +1,8 @@
 function REBayes(L)
     @rput L;
     R"require(REBayes);
-    t_rebayes = system.time(res <- KWDual(L, rep(1,dim(L)[2]), rep(1,dim(L)[1])/dim(L)[1]))[3];
+    t_rebayes = system.time(res <- KWDual(L, rep(1,dim(L)[2]),
+                                     rep(1,dim(L)[1])/dim(L)[1]))[3];
     res$f[res$f < 1e-3] = 0
     x_rebayes = res$f / sum(res$f)"
     @rget x_rebayes;
