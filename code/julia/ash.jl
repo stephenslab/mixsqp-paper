e854cfed5741302ce2797e56e7c9aaaef38c0b3a
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-05-25 12:49:40 -0500

    Revising ash funtion and ash_computation_time notebook.
    Former-commit-id: 571833cd298273340454af4b662d48dc3d89dd78
    Former-commit-id: 66f6b8c56360a02beb93aef65b5121a0032ab8fb

--- code/julia/ash.jl
+++ code/julia/ash.jl
@@ -1,48 +1,49 @@
-function ash(x,s; method = "mixSQP", mult = 1.3, lowrank = "svd")
+# Basic Julia implementation of adaptive shrinkage with a normal
+# likelihood and normal mixture components.
+function ash(x, s; method = "mixSQP", gridmult = 1.3, lowrank = "svd")
 
-    # Compute the n x m likelihood matrix.
-    tic();
-    sd      = autoselectmixsd(x,s,gridmult = mult);
+  # Compute the n x m conditional likelihood matrix.
+  out, t1, bytes, gctime, memallocs = @timed if true
+    sd      = autoselectmixsd(x,s,gridmult = gridmult);
     log_lik = normlikmatrix(x,s,sd = sd);
-    s2      = s.^2;
-    t1      = toq();
+  end
     
-    # Fit the model.
-    if method == "mixSQP"
-        temp = mixSQP_time(log_lik, pqrtol = 1e-8, lowrank = lowrank);
-        t3 = temp[3];
-        t2 = temp[2];
-        p = temp[1];
-    elseif method == "REBayes"
-        temp = REBayes(log_lik);
-        t2 = temp[2];
-        p = temp[1];
-    else
-        error("Error")
-    end
+  # Fit the model.
+  if method == "mixSQP"
+    temp = mixSQP_time(log_lik, pqrtol = 1e-8, lowrank = lowrank);
+    t3 = temp[3];
+    t2 = temp[2];
+    p  = temp[1];
+  elseif method == "REBayes"
+    temp = REBayes(log_lik);
+    t2 = temp[2];
+    p = temp[1];
+  else
+    error("Error")
+  end
     
-    tic();
+  tic();
     
     # Exploit sparsity.
     ind = find(p .> 0);
     ps2 = sd[ind].^2;
     
     # Posterior calculations.
-    temp = s2 .+ ps2';
-    comp_post_mean = (x * ps2') ./ temp;
-    comp_post_sd2 = (s2 * ps2') ./ temp;
+    s2   = s.^2;
+    t               = s2 .+ ps2';
+    comp_post_mean  = (x * ps2') ./ t;
+    comp_post_sd2   = (s2 * ps2') ./ t;
     comp_post_mean2 = comp_post_sd2 + comp_post_mean.^2;
-    comp_post_prob = log_lik[:,ind] .* p[ind]';
-    comp_post_prob = comp_post_prob ./ sum(comp_post_prob,2);
-    post_mean = sum(comp_post_prob .* comp_post_mean,2);
-    post_mean2 = sum(comp_post_prob .* comp_post_mean2,2);
+    comp_post_prob  = log_lik[:,ind] .* p[ind]';
+    comp_post_prob  = comp_post_prob ./ sum(comp_post_prob,2);
+    post_mean       = sum(comp_post_prob .* comp_post_mean,2);
+    post_mean2      = sum(comp_post_prob .* comp_post_mean2,2);
     t4 = toq();
     
     # return posterior first/second moments
-    if method == "mixSQP"
-        return post_mean, post_mean2, log_lik, p, [t1;t2;t3;t4]
-    else
-        return post_mean, post_mean2, log_lik, p, [t1;t2;t4]
-    end
-
+  if method == "mixSQP"
+    return post_mean, post_mean2, log_lik, p, [t1;t2;t3;t4]
+  else
+    return post_mean, post_mean2, log_lik, p, [t1;t2;t4]
+  end
 end
