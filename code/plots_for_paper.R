a7c628aebfa9cff20500ef28985b18c90deea6bc
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-01-21 12:21:38 -0600

    Changed file names for some figures.
    Former-commit-id: 01903d81f42a121d3fa48361ebaade3bb809ee22
    Former-commit-id: 737680558074ee40afcaaaa5e2d5e64e1c195bcb

--- code/plots_for_paper.R
+++ code/plots_for_paper.R
@@ -416,7 +416,8 @@ p20 <- ggplot(data = dat8_1[c(1,3,5:17,18,20,22:34,35,37,39:51),]) +
 # SAVE PLOTS AS PDFs
 # ------------------
 ggsave("../output/compare-formulations.pdf",p1,height = 4,width = 7)
-ggsave("../output/F2.pdf",plot_grid(p3,p4),height = 4,width = 9)
+ggsave("../output/sqp-lowrank-approx.pdf",plot_grid(p3,p4),
+       height = 4,width = 9)
 ggsave("../output/F3.pdf",plot_grid(p13,p14,p15,nrow = 1),
        height = 4,width = 12)
 ggsave("../output/F4.pdf",plot_grid(p9,p10,p11,nrow = 1),height = 4,
