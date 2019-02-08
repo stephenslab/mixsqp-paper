685c34b9d876d1bf3a3d3ee1e83210b6e7c50879
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-02-08 13:04:42 -0600

    Made some improvements to the plots comparing mix-SQP vs. first-order methods.
    Former-commit-id: bff45cd10e3435d80a4b4cb87da8feb1f2360606
    Former-commit-id: a42a0b471b5fe097a27d7b2e50381908b6a76196

--- code/create_new_plots.R
+++ code/create_new_plots.R
@@ -1,13 +1,22 @@
 library(ggplot2)
 library(cowplot)
-dat <- read.csv("../output/simcompare2.csv")
-# dat$robj <- pmax(1e-4,dat$robj)
+dat <- read.csv("../output/simcompare3.csv")
+dat.matlab <- read.csv("../output/pg-n=1000-m=200.csv",header = FALSE)
+dat.matlab <- cbind(dat.matlab,"minConf_SPG")
+names(dat.matlab) <- c("time","robj","method")
+f <- min(dat$obj - dat$robj)
+dat.matlab$robj <- dat.matlab$robj - f
+dat <- rbind(dat[c("time","robj","method")],
+             dat.matlab)
+dat$robj <- pmax(1e-4,dat$robj)
 p <- ggplot(dat,aes(x = time,y = robj,color = method)) +
     geom_point() +
     geom_line() +
+    xlim(c(0,0.5)) +
     scale_y_log10() +
     scale_color_manual(values = c("darkorange","darkblue","dodgerblue",
-                                  "firebrick")) +
+                                  "firebrick","magenta")) +
     labs(x = "runtime (seconds)",
-         y = "distance from minimum")
-   
+         y = "distance from minimum",
+         title = "m=200")
+
