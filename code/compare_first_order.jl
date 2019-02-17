2faf1d868c66295003c1e3076e32f271a1cffb74
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-02-17 10:01:23 -0600

    Some improvements to compare_first_order.jl.
    Former-commit-id: acad320b68d3a31238f1339e3d22e3b59ac85479
    Former-commit-id: e71c37061f70015f7d858ea3d5b5aca1ab10aaf8

--- code/compare_first_order.jl
+++ code/compare_first_order.jl
@@ -1,7 +1,16 @@
-# Generate the data sets used in the comparisons of mix-SQP vs. the
-# first-order methods (EM and projected gradient).
+# This is the script used to generate the results comparing mix-SQP
+# vs. the EM algorithm. The results for the projected gradient method
+# are generated separately in run_pg.m after first running this
+# script. To run on the RCC cluster, I set up my environment with the
+# following commands:
+#
+# sinteractive --partition=broadwl --mem=8G
+# module load julia/0.6.2
+#
+# Note that I also ran this script with m = 200 and m = 2000.
 n = 20000;
-m = 200;
+m = 20;
+matrixfile = "simdata-n=20000-m=20.csv";
 
 using Distributions
 using LowRankApprox
@@ -13,21 +22,41 @@ include("mixSQP.jl");
 
 # Generate the matrix.
 srand(2019);
+@printf "Generating %d x %d data matrix.\n" n m
 z  = normtmixdatasim(n);
 sd = autoselectmixsd(z,nv = m);
 L  = normlikmatrix(z,sd = sd);
 
-# Run the methods.
-xem, fem, tem = mixEM(L,maxiter = 10000,tol = 1e-6);
-outsqp1 = mixSQP(L,lowrank = "none", eps = 1e-8, maxqpiter = 200,
-                 maxiter = 200, verbose = false, nullprior = 0);
-outsqp2 = mixSQP(L,lowrank = "qr", eps = 1e-8, maxqpiter = 200,
-                 maxiter = 200, verbose = false, nullprior = 0);
+# Save the matrix to a CSV file.
+@printf "Writing data to CSV file.\n"
+writecsv(matrixfile,L);
+
+# Run the methods for a small number of iterations to precompile the
+# code.
+@printf "Precompiling EM and mixSQP code.\n"
+xem, fem, tem = mixEM(L,maxiter = 10);
+outsqp1 = mixSQP(L,lowrank = "none",eps = 1e-8,maxqpiter = m,
+                 maxiter = 20,verbose = false);
+outsqp2 = mixSQP(L,lowrank = "qr", eps = 1e-8, maxqpiter = m,
+                 maxiter = 200,verbose = false);
+
+# Run the EM algorithm.
+@printf "Fitting model using EM.\n"
+@time xem, fem, tem = mixEM(L,maxiter = 10000,tol = 1e-6);
+
+# Run mix-SQP with no approximation to the input matrix.
+@printf "Fitting model using mix-SQP with exact L.\n"
+outsqp1 = mixSQP(L,lowrank = "none",eps = 1e-8,maxqpiter = m,
+                 maxiter = 200,verbose = false);
+
+# Run the mix-SQP with a low rank (truncated QR) approximation to the
+# input matrix.
+@printf "Fitting model using mix-SQP with approximate L.\n"
+outsqp2 = mixSQP(L,lowrank = "qr", eps = 1e-8, maxqpiter = m,
+                 maxiter = 200,verbose = false);
 
 # Compare the quality of the solutions.
 @printf "Objective at SQP1 solution: %0.12e\n" mixobjective(L,outsqp1["x"])
 @printf "Objective at SQP2 solution: %0.12e\n" mixobjective(L,outsqp2["x"])
 @printf "Objective at EM   solution: %0.12e\n" mixobjective(L,xem)
 
-# Save the matrix to a CSV file.
-writecsv("simdata-n=20000-m=200.csv",L);
