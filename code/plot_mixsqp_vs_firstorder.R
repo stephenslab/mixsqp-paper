307ffd50bef0d5902fe1015190c1d41b425a4780
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-02-18 13:20:18 -0600

    Wrote first draft of plot_mixsqp_vs_firstorder.R.
    Former-commit-id: 486f183c401c1da7b01a3bbc8f351ded5e7fa756
    Former-commit-id: f739527b3553ffaadf88149fc57c9848a2c4688d

---
+++ code/plot_mixsqp_vs_firstorder.R
@@ -0,0 +1,36 @@
+# TO DO: Explain here what this script is for, and how to use it.
+library(ggplot2)
+library(cowplot)
+
+# Read and prepare the data for plotting.
+dat1 <- read.csv("../output/mixsqp-exact-n=20000-m=20.csv.gz",header = FALSE)
+dat2 <- read.csv("../output/mixsqp-approx-n=20000-m=20.csv.gz",header = FALSE)
+dat3 <- read.csv("../output/em-n=20000-m=20.csv.gz",header = FALSE)
+dat4 <- read.csv("../output/pg-n=20000-m=20.csv.gz",header = FALSE)
+dat  <- rbind(cbind(data.frame(method    = "mix-SQP-exact",
+                               objective = dat1[[1]],
+                               runtime   = cumsum(dat1[[2]]))),
+              cbind(data.frame(method    = "mix-SQP-approx",
+                               objective = dat2[[1]],
+                               runtime   = cumsum(dat2[[2]]))),
+              cbind(data.frame(method    = "EM",
+                               objective = dat3[[1]],
+                               runtime   = cumsum(dat3[[2]]))),
+              cbind(data.frame(method   = "PG",
+                               objective = dat4[[1]],
+                               runtime   = cumsum(dat4[[2]]))))
+rm(dat1,dat2,dat3,dat4)
+
+# Find the distance between the current solutions and the best solution.
+f   <- min(dat$objective)
+dat <- transform(dat,objective = objective - f + 1e-8)
+
+# Create the plot comparing performance of the methods over time.
+p <- ggplot(dat,aes(x = runtime,y = objective,color = method)) +
+  geom_line() +
+  geom_point() +
+  xlim(c(0,10)) +
+  scale_y_log10() +
+  labs(x = "runtime (seconds)",
+       y = "distance from minimum",
+       title = "n=20,000, m=20")
