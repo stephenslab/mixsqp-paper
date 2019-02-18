f13f05e0e65b64ffff931fde14ed926e91cec195
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-02-18 17:13:17 -0600

    Added results on 20,000 x 200 simulated data set.
    Former-commit-id: 4812ebf3e7e17722e3f4f893faaf13626185760d
    Former-commit-id: 9e1223dd93984ce158855d1e89ffe08f87777eb7

--- code/compare_first_order.jl
+++ code/compare_first_order.jl
@@ -7,14 +7,14 @@
 # sinteractive --partition=broadwl --mem=8G
 # module load julia/0.6.2
 #
-# Note that we ran this script with m = 20 and m = 200.
+# Note that we ran this script with m = 20, m = 200 and m = 2000.
 #
 n = 20000;
-m = 2000;
-matrixfile      = "simdata-n=20000-m=2000.csv";
-outfile_mixsqp1 = "mixsqp-exact-n=20000-m=2000.csv";
-outfile_mixsqp2 = "mixsqp-approx-n=20000-m=2000.csv";
-outfile_em      = "em-n=20000-m=2000.csv";
+m = 200;
+matrixfile      = "simdata-n=20000-m=200.csv";
+outfile_mixsqp1 = "mixsqp-exact-n=20000-m=200.csv";
+outfile_mixsqp2 = "mixsqp-approx-n=20000-m=200.csv";
+outfile_em      = "em-n=20000-m=200.csv";
 
 using Distributions
 using LowRankApprox
