b5d253aaa5119b9f11f254a85f186dea55117b62
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-02-18 21:25:43 -0600

    Some improvements to the R plotting code.
    Former-commit-id: ebe0a10ada04f2dcb495d3245ee4d1f42c0fd591
    Former-commit-id: 6d5818535a791da489ab98471244083c25ef55e3

--- code/plot_mixsqp_vs_firstorder.R
+++ code/plot_mixsqp_vs_firstorder.R
@@ -4,10 +4,10 @@ library(ggplot2)
 library(cowplot)
 
 # Read and prepare the data for plotting.
-dat1 <- read.csv("../output/mixsqp-exact-n=20000-m=20.csv.gz",header = FALSE)
-dat2 <- read.csv("../output/mixsqp-approx-n=20000-m=20.csv.gz",header = FALSE)
-dat3 <- read.csv("../output/em-n=20000-m=20.csv.gz",header = FALSE)
-dat4 <- read.csv("../output/pg-n=20000-m=20.csv.gz",header = FALSE)
+dat1 <- read.csv("../output/mixsqp-exact-n=20000-m=200.csv.gz",header = FALSE)
+dat2 <- read.csv("../output/mixsqp-approx-n=20000-m=200.csv.gz",header = FALSE)
+dat3 <- read.csv("../output/em-n=20000-m=200.csv.gz",header = FALSE)
+dat4 <- read.csv("../output/pg-n=20000-m=200.csv.gz",header = FALSE)
 dat  <- rbind(cbind(data.frame(method    = "mix-SQP-exact",
                                objective = dat1[[1]],
                                runtime   = cumsum(dat1[[2]]))),
@@ -25,14 +25,16 @@ rm(dat1,dat2,dat3,dat4)
 # Find the distance between the current solutions and the best solution.
 f   <- min(subset(dat,method == "mix-SQP-exact" |
                       method == "mix-SQP-approx")$objective)
-dat <- transform(dat,objective = objective - f + 1e-8)
+dat <- transform(dat,objective = pmax(objective - f,1e-8))
 
 # Create the plot comparing performance of the methods over time.
+# For the 20,000 x 20 data set, use xlim(c(0,0.5)).
+# For the 20,000 x 200 data set, use xlim(c(0,8)).
 p <- ggplot(dat,aes(x = runtime,y = objective,color = method)) +
   geom_line() +
   geom_point() +
-  xlim(c(0,12)) +
+  xlim(c(0,8)) +
   scale_y_log10() +
   labs(x = "runtime (seconds)",
        y = "distance from minimum",
-       title = "n=20,000, m=20")
+       title = "n=20,000, m=200")
