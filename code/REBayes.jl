b0357872e087343ff592fbda5e6649a74a0ac708
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-08-21 13:54:24 -0500

    Revised mixSQP_REBayes_EM analysis for Julia 1.1.1.
    Former-commit-id: 79ccb08193a33c34ead6ca6de0bf64256aa97979
    Former-commit-id: da399dad088c54373bc00c66cf127bcfc95cb548

--- code/REBayes.jl
+++ code/REBayes.jl
@@ -1,13 +1,14 @@
+# Fit mixture model using MOSEK interior-point solver, called by the
+# KWDual function from the REBayes package.
 function REBayes(L)
   @rput L;
   R"library(REBayes);
-    n           <- nrow(L)
-    k           <- ncol(L)
-    timing      <- system.time(out <- KWDual(L,rep(1,k),rep(1,n)/n));
-    timing      <- timing[3];
-    x           <- out$f;
-    x[x < 1e-3] <- 0;
-    x           <- x/sum(x);"
+    n      <- nrow(L)
+    k      <- ncol(L)
+    timing <- system.time(out <- KWDual(L,rep(1,k),rep(1,n)/n));
+    timing <- timing[3];
+    x      <- out$f;
+    x      <- x/sum(x);"
   @rget x;
   @rget timing;
   return x, timing
