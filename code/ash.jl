e3dbb65657ac94cee3bdd902dc8b30a76d355b06
Peter Carbonetto <peter.carbonetto@gmail.com>
2019-08-21 14:15:37 -0500

    Updated ash_computation_time demo for Julia 1.1.1.
    Former-commit-id: 1a39601ea6f53afb632a853cdfa30c2f584b8288
    Former-commit-id: fc858f3fc9c94394efe0ca95fb92ed0adac8d9f1

--- code/ash.jl
+++ code/ash.jl
@@ -1,16 +1,14 @@
 # Basic Julia implementation of adaptive shrinkage with normal
 # likelihood and mixture-of-centered-normals prior. The outputs are
-# the fitted mixture weights (x), the standard deviations of the
-# normal mixture components (sd), the posterior mean effect estimates
-# (post_mean), and the runtime of the likelihood computations
-# (t_likelihood), model fitting (t_fit) and posterior computations
-# (t_posterior).
+# the fitted mixture weights (x) and the runtime of the likelihood
+# computations (t_likelihood), model fitting (t_fit) and posterior
+# computations (t_posterior).
 function ash(x, s; method = "mixSQP", gridmult = 1.3, lowrank = "svd",
              pqrtol = 1e-8)
 
   # Select the variances of the normal mixture components, then
   # compute the n x m conditional likelihood matrix.
-  out, t_likelihood, bytes, gctime, memallocs = @timed if true
+  out, t_likelihood, bytes, gctime, memallocs = @timed begin
     sd = autoselectmixsd(x,s,gridmult = gridmult);
     L  = normlikmatrix(x,s,sd = sd);
   end
@@ -25,20 +23,22 @@ function ash(x, s; method = "mixSQP", gridmult = 1.3, lowrank = "svd",
   end
     
   # Posterior calculations.
-  out, t_posterior, bytes, gctime, memallocs = @timed if true
-    ind = find(w .> 0);
+  out, t_posterior, bytes, gctime, memallocs = @timed begin
+    ind = findall(w .> 0);
     ps2 = sd[ind].^2;
     s2  = s.^2;
     t   = s2 .+ ps2';
     comp_post_mean  = (x * ps2') ./ t;
     comp_post_sd2   = (s2 * ps2') ./ t;
-    comp_post_mean2 = comp_post_sd2 + comp_post_mean.^2;
+    comp_post_mean2 = comp_post_sd2 .+ comp_post_mean.^2;
     comp_post_prob  = L[:,ind] .* w[ind]';
-    comp_post_prob  = comp_post_prob ./ sum(comp_post_prob,2);
-    post_mean       = sum(comp_post_prob .* comp_post_mean,2);
-    post_mean2      = sum(comp_post_prob .* comp_post_mean2,2);
+    comp_post_prob  = comp_post_prob ./ sum(comp_post_prob,dims = 2);
+    post_mean       = sum(comp_post_prob .* comp_post_mean,dims = 2);
+    post_mean2      = sum(comp_post_prob .* comp_post_mean2,dims = 2);
   end
     
-  return Dict([("x",x), ("timing-likelihood",t_likelihood),
-               ("timing-fit",t_fit), ("timing-posterior",t_posterior)])
+  return Dict([("x",x),
+               ("timing-likelihood",t_likelihood),
+               ("timing-fit",t_fit),
+               ("timing-posterior",t_posterior)])
 end
